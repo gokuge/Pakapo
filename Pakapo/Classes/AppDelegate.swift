d633432a01e7418a94763dfc52b9af01cd70ff32
gokuge <57989544+gokuge@users.noreply.github.com>
2019-12-06 16:56:14 +0900

    Initial Commit

---
+++ Pakapo/Classes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pakapo
+//
+//  Created by gokuge on 2019/12/06.
+//  Copyright © 2019 gokuge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
